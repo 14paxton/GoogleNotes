ca8c26727e911d0797fd1f9bdd3336b344813eb0
Brandon003842 <brandon003842@veridiancu.org>
2023-05-03 09:26:56 -0500

    Update WebRequest.cs

--- Functions/DialogFlow/WebRequest.cs
+++ Functions/DialogFlow/WebRequest.cs
@@ -14,68 +14,70 @@ using System.Threading.Tasks;
 
 namespace DialogFlow;
 
-public class Function : IHttpFunction {
+public class Function : IHttpFunction
+{
     private readonly ILogger _log;
 
     public Function(ILogger<Function> log) =>
             _log = log;
 
-    public async Task HandleAsync(HttpContext context){
+    public async Task HandleAsync(HttpContext context)
+    {
         HttpRequest httpRequest = context.Request;
         HttpResponse httpResponse = context.Response;
 
         WebhookResponse response = new()
-            {
-                FulfillmentResponse = new WebhookResponse.Types.FulfillmentResponse()
-            };
+        {
+            FulfillmentResponse = new WebhookResponse.Types.FulfillmentResponse()
+        };
 
         using TextReader reader = new StreamReader(httpRequest.Body);
         string text = await reader.ReadToEndAsync();
         _log.LogInformation($"http request body- WebhookRequest : {text}");
 
         JsonParser.Settings parserSettings = JsonParser.Settings.Default.WithIgnoreUnknownFields(true);
-        JsonParser          jParser        = new (parserSettings);
-        WebhookRequest      webhookRequest = jParser.Parse <WebhookRequest>(text);
-        SessionInfo         sessionInfo    = webhookRequest.SessionInfo;
-        string              tag            = webhookRequest.FulfillmentInfo.Tag;
+        JsonParser jParser = new(parserSettings);
+        WebhookRequest webhookRequest = jParser.Parse<WebhookRequest>(text);
+        SessionInfo sessionInfo = webhookRequest.SessionInfo;
+        string tag = webhookRequest.FulfillmentInfo.Tag;
         sessionInfo.Parameters.TryGetValue(key: "name", value: out var nameParameterValue);
 
         _log.LogInformation($"webhookrequest : {webhookRequest}");
 
-        if(tag == "comm100-customfields" && !string.IsNullOrEmpty(nameParameterValue?.ToString()))
+        if (tag == "comm100-customfields" && !string.IsNullOrEmpty(nameParameterValue?.ToString()))
         {
             try
             {
                 RestClientOptions options = new("https://api12.comm100.io")
-                    {
-                        MaxTimeout = -1
-                    };
+                {
+                    MaxTimeout = -1
+                };
 
                 RestClient client = new RestClient(options);
                 RestRequest restRequest = new RestRequest("/v4/livechat/visitors?siteId=75000104&include=contactIdentity&include=chatAgent&include=department");
                 restRequest.AddHeader(name: "Authorization", value: "Basic [redacted]");
                 RestResponse comm100JsonResponse = await client.ExecuteAsync(request: restRequest);
                 List<JsonElement> visitorList = JsonSerializer.Deserialize<List<JsonElement>>(comm100JsonResponse.Content ?? string.Empty);
-                List<JsonElement> filteredList = visitorList.FindAll( visitor => visitor.GetProperty("name").GetString() == nameParameterValue.StringValue);
+                List<JsonElement> filteredList = visitorList.FindAll(visitor => visitor.GetProperty("name").GetString() == nameParameterValue.StringValue);
 
-                if(filteredList.Count == 1)
+                if (filteredList.Count == 1)
                 {
                     response.SessionInfo = new SessionInfo();
                     List<JsonElement> customFields = filteredList[index: 0].GetProperty("customFields").Deserialize<List<JsonElement>>();
 
-                    foreach(JsonElement fieldObject in customFields)
+                    foreach (JsonElement fieldObject in customFields)
                     {
                         string key = fieldObject.GetProperty("name").GetString()?.Trim().ToLower().Replace(oldValue: " ", newValue: "-");
                         Value value = Value.ForString(fieldObject.GetProperty("value").GetString());
 
-                        if(!string.IsNullOrEmpty(key))
+                        if (!string.IsNullOrEmpty(key))
                         {
                             response.SessionInfo.Parameters.Add(key: key, value: value);
                         }
                     }
                 }
             }
-            catch(Exception parseException)
+            catch (Exception parseException)
             {
                 Console.Write(value: parseException);
                 _log.LogError($"error : {parseException}");
